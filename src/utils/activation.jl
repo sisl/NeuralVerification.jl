7ca9f81507272285a7277c4684480297643fa884
changliuliu <changliu.liu@le.com>
2018-09-08 12:55:41 -0700

    add identical activation

--- src/utils/activation.jl
+++ src/utils/activation.jl
@@ -3,10 +3,12 @@ abstract type ActivationFunction end
 struct GeneralAct <: ActivationFunction end
 struct ReLU <: ActivationFunction end
 struct Max <: ActivationFunction end
+struct Id <: ActivationFunction end
 
 (f::GeneralAct)(x) = f(x)
 (f::ReLU)(x) = max.(x,0)
 (f::Max)(x) = max(maximum(x),0)
+(f::Id)(x) = x
 
 
 
