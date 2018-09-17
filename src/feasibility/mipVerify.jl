74967089861d32607d275fbac60070aae7e1831d
changliuliu <changliu.liu@le.com>
2018-09-17 16:33:00 -0700

    implement encode for MIPVerify, currently only support ReLU

---
+++ src/feasibility/mipVerify.jl
@@ -0,0 +1,50 @@
+# MIPVerify
+# 	Include maximum activation
+# 	Include presolve
+# Only take half space input constraint
+# Computes the allowable radius of input perturbations
+struct MIPVerify{O<:AbstractMathProgSolver} <: Feasibility
+	optimizer::O
+end
+
+function encode(solver::MIPVerify, model::Model, problem::Problem)
+	bounds = get_bounds(problem)
+	neurons, deltas = init_nnet_vars(solver, model, problem.network)
+    add_complementary_output_constraint(model, problem.output, last(neurons))
+    for (i, layer) in enumerate(problem.network.layers)
+    	(W, b, act) = (layer.weights, layer.bias, layer.activation)
+        before_act = W * neurons[i] + b
+        before_act_center = W * bounds[i].center + b
+        before_act_radius = zeros(size(W,1))
+        for j in 1:size(W, 1)
+        	before_act_radius[j] = sum(abs.(W[j, :]) .* bounds[i].radius)
+        end
+        before_act_rectangle = Hyperrectangle(before_act_center, before_act_radius)
+        lower = low(before_act_rectangle)
+        upper = high(before_act_rectangle)
+
+        # For now assume only ReLU activation
+        for j in 1:length(layer.bias) # For evey node
+        	if lower[j] >= 0.0
+        		@constraint(model, neurons[i+1][j] == before_act[j])
+        	elseif upper[j] <= 0.0 
+        		@constraint(model, neurons[i+1][j] == 0.0)
+        	else
+	            ubounds = lbounds + dy[j]
+            	@constraints(model, begin
+                                    neurons[i+1][j] .>= before_act[j]
+                                    neurons[i+1][j] .<= upper[j] * deltas[i+1][j]
+                                    neurons[i+1][j]  >= 0.0
+                                    neurons[i+1][j]  <= before_act[j] - lower[j] * (1 - deltas[i+1][j])
+                                end)
+        	end
+        end
+    end
+
+    # Need to specify objective here
+    # @objective(model, Min, norm(first(neurons) - problem.input.center))
+end
+
+function interpret_result(solver::MIPVerify, status)
+	return Result(:True)
+end
