39219a3f73bd5b7084e5aa28b0464f87e5ee754e
tomerarnon <tomerarnon@users.noreply.github.com>
2018-09-24 00:10:37 -0700

    stepping stone commit for convDual

--- src/feasibility/convDual.jl
+++ src/feasibility/convDual.jl
@@ -19,7 +19,7 @@ function dual_cost(solver::ConvDual, network::Network, input::Hyperrectangle{N},
     @assert iszero(input.radius - input.radius[1]) "input.radius must be uniform. Got $(input.radius)"
 
     layers = network.layers
-    L, U, act_pattern = get_bounds(network, input.center, input.radius[1])
+    L, U = get_bounds(network, input.center, input.radius[1])
     v, J = tosimplehrep(output)
 
     for i in reverse(1:length(layers))
@@ -39,7 +39,7 @@ modifies v and returns J
 function backprop!(v, u, l)
     J = 0.0
     for j in 1:length(v)
-        val = relaxed_ReLU(l[j], u[j])
+        val = relaxed_ReLU_slope(l[j], u[j])
         if val < 1.0 # if val is 1, it means ReLU result is identity so do not update (NOTE is that the right reasoning?)
             v[j] = abs(v[j]) * val
             J += v[j] * l[j]
@@ -49,87 +49,77 @@ function backprop!(v, u, l)
 end
 
 
+# Forward_network and forward_layer:
 # This step is similar to reachability method
-function get_bounds(nnet::Network, input::Vector{Float64}, epsilon::Float64)
-    n_layer = length(nnet.layers)
+function get_bounds(nnet::Network, input::Vector{Float64}, ϵ::Float64)
+    layers  = nnet.layers
+    n_layer = length(layers)
+
     l = Vector{Vector{Float64}}(n_layer)
     u = Vector{Vector{Float64}}(n_layer)
-    act_pattern = Vector{Vector{Float64}}(n_layer)
+    γ = Vector{Vector{Float64}}(n_layer)
+    μ = Vector{Vector{Vector{Float64}}}(n_layer)
 
-    gamma = Vector{Vector{Float64}}(n_layer)
-    mu = Vector{Vector{Vector{Float64}}}(n_layer)
-
-    v1 = nnet.layers[1].weights'
-    gamma[1] = nnet.layers[1].bias
+    W, b = layers[1].weights, layers[1].bias
+    v1   = W'
+    γ[1] = b
 
     # Bounds for the first layer
-    output = nnet.layers[1].weights * input + nnet.layers[1].bias
-    n_output = length(output)
-    l[1] = zeros(n_output)
-    u[1] = zeros(n_output)
-    for i in 1:n_output
-        l[1][i] = output[i] - epsilon * sum(abs.(nnet.layers[1].weights[i, :])) #TODO check [i, :] vs [:, i]
-        u[1][i] = output[i] + epsilon * sum(abs.(nnet.layers[1].weights[i, :]))
-    end
+    out1 = W * input + b
+    Δ    = ϵ * sum(abs.(W), 2)  #TODO check sum(, 1) vs sum(, 2)
+    l[1] = out1 - Δ
+    u[1] = out1 + Δ
 
     for i in 2:n_layer
-        act_pattern[i-1], D = get_activation(l[i-1], u[i-1])
+
+        W, b = layers[i].weights, layers[i].bias
+        n_input  = length(l[i-1])
+        n_output = length(b)
+
+        # Form Ι⁻ᵢ, Ι⁺ᵢ, Ιᵢ; Form D
+        prev_layer_ReLU_slopes = relaxed_ReLU_slope.(l[i-1], u[i-1])
+        D = diagm(prev_layer_ReLU_slopes)   # a matrix whose diagonal values are the relaxed_ReLU values (maybe should be sparse?)
 
         # Initialize new terms
-        gamma[i] = nnet.layers[i].bias
-
-        n_input = length(act_pattern[i-1])
-        n_output = length(nnet.layers[i].bias)
-
-        mu[i] = Vector{Vector{Float64}}(n_input)
+        μ[i] = [zeros(n_output) for j in 1:n_input]
         for j in 1:n_input
-            if act_pattern[i-1][j] == 0
-            #if relaxed_ReLU(l[i-1], u[i-1]) == -1 # equivalent
-                mu[i][j] = nnet.layers[i].weights * D[:, j]
-            else
-                mu[i][j] = zeros(n_output)
+            if prev_layer_ReLU_slopes[j] == 0 # negative part
+                μ[i][j] = W * D[:, j]
             end
         end
+        γ[i] = b
 
-        # Propagate existiing terms
-        for j in 1:i-1
-            ReLUed_weights = nnet.layers[i].weights * D
-            if j > 1
-                mu[j] = [ReLUed_weights * m for m in mu[j]]
-            end
-            gamma[j] = ReLUed_weights * gamma[j]
-        end
-        # (ABC')' = CB'A'
-        v1 = v1 * D' * nnet.layers[i].weights'
+        # Propagate existing terms
+        DW = D*W'
+        γ = [γ[j]*DW              for j in 1:i-1]
+        μ = [[m*DW for m in μ[j]] for j in 2:i-1]
+
+        v1 = v1 * DW
 
         # Compute bounds
-        phi = v1' * input + sum(gamma[j] for j in 1:i)
+        ψ = input' * v1 + sum(γ[j] for j in 1:i)
 
-        vertical_sums_on_v1 = vec(sum(abs.(v1), 1)) # sum down the columns after abs
-        neg, pos = all_neg_pos_sums(i, n_output, act_pattern, l, mu)
-        l[i] = phi .- epsilon * vertical_sums_on_v1 + neg
-        u[i] = phi .+ epsilon * vertical_sums_on_v1 - pos
+        colsums_v1 = vec(sum(abs.(v1), 1)) # sum down the columns after abs
+        neg, pos = all_neg_pos_sums(i, n_output, prev_layer_ReLU_slopes, l, μ)
+        l[i] = ψ - ϵ*colsums_v1 + neg
+        u[i] = ψ + ϵ*colsums_v1 - pos
     end
 
-    act_pattern[n_layer], D = get_activation(l[n_layer], u[n_layer])
-
-    return (l, u, act_pattern)
+    return l, u
 end
 
 # TODO rename function and inputs
-function all_neg_pos_sums(i, n_output, act_pattern, l, mu)
+function all_neg_pos_sums(i, n_output, slopes, l, mu)
     neg = zeros(n_output)
     pos = zeros(n_output)
     # Need to debug
     for j in 1:i-1
         for k in 1:length(mu[j+1])
-            if act_pattern[i-1][k] == 0  # relaxed_ReLU(l, u) !∈ (0.0, 1.0)
-                muvals = mu[j+1][k]
-                for (ii, val) in enumerate(muvals)
-                    if     val < 0    neg[ii] += l[j][k] * -val
-                    elseif val > 0    pos[ii] += l[j][k] *  val
-                    end
-                end
+            if 0 < slopes[k] < 1  # if in the triangle region
+                posind = mu[j+1][k] .> 0
+
+                neg .+= l[j][k] .* -mu[j+1][k] .* !posind  # multiply by boolean to set the undesired values to 0.0
+                pos .+= l[j][k] .*  mu[j+1][k] .* posind
             end
         end
     end
@@ -137,19 +127,17 @@ function all_neg_pos_sums(i, n_output, act_pattern, l, mu)
 end
 
 
-
-
-function get_activation(l::Vector{Float64}, u::Vector{Float64})
-    act_pattern = get_activation.(l, u)
-    D  = spdiagm(relaxed_ReLU.(l, u))  # a sparse matrix whose diagonal values are the relaxed_ReLU values
-    return (act_pattern, D)
+function get_activation(reluslope::Float64)
+    reluslope == 0.0 && return (:neg)
+    reluslope == 1.0 && return (:pos)
+    return (:tri)
 end
 function get_activation(l::Float64, u::Float64)
-    u <= 0.0 && return -1
-    l >= 0.0 && return 1
-    return 0
+    u <= 0.0 && return (:neg)
+    l >= 0.0 && return (:pos)
+    return (:tri)
 end
-function relaxed_ReLU(l::Float64, u::Float64)
+function relaxed_ReLU_slope(l::Float64, u::Float64)
     u <= 0.0 && return 0.0
     l >= 0.0 && return 1.0
     return u / (u - l)
