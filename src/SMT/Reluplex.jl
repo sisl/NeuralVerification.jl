0e2fb6668dbf6f57e20d8074797bb77e417a2459
Christopher Lazarus <clazarusg@gmail.com>
2018-10-09 02:08:58 -0700

    clean progress
    about to integrate into main loop
    TODO: add solver argument and fix import issues with new package design

--- src/SMT/Reluplex.jl
+++ src/SMT/Reluplex.jl
@@ -1,39 +1,103 @@
 struct Reluplex{O<:AbstractMathProgSolver} <: SMT
     optimizer::O
-    m::Float64 # The big M in the linearization
 end
 
-Reluplex(x) = Reluplex(x, 1000.0)
+function init_nnet_vars(model::Model, network::Network)
+    layers = network.layers
+    #input layer and last layer have b_vars because they are unbounded
+    b_vars = Vector{Vector{Variable}}(length(layers) + 1) # +1 for input layer
+    #f_vars are always positive and used as front for ReLUs
+    f_vars = Vector{Vector{Variable}}(length(layers) -1)
+    
+    input_layer_n = size(first(layers).weights, 2)
+    all_layers_n  = [length(l.bias) for l in layers]
+    insert!(all_layers_n, 1, input_layer_n)
 
-function interpret_result(solver::Reverify, status, neurons)
-    if status == :Optimal
-        return Result(:False, getvalue(neurons))
-    end
-    if status == :Infeasible
-        return Result(:True)
-    end
-    return Result(:Undertermined)
-end
-#=
-Encode problem as an MIP following Reverify algorithm
-=#
-function encode(solver::Reverify, model::Model, problem::Problem)
-    neurons, deltas = init_nnet_vars(solver, model, problem.network)
-    add_input_constraint(model, problem.input, first(neurons))
-    add_complementary_output_constraint(model, problem.output, last(neurons))
-    for (i, layer) in enumerate(problem.network.layers)
-        lbounds = layer.weights * neurons[i] + layer.bias
-        dy = solver.m*(deltas[i+1])  # TODO rename variable
-        ubounds = lbounds + dy
-        for j in 1:length(layer.bias)
-            @constraints(model, begin
-                                    neurons[i+1][j] >= lbounds[j]
-                                    neurons[i+1][j] <= ubounds[j]
-                                    neurons[i+1][j] >= 0.0
-                                    neurons[i+1][j] <= solver.m-dy[j]
-                                end)
+    for (i, n) in enumerate(all_layers_n)
+        b_vars[i] = @variable(model, [1:n]) # To do: name the variables
+        if 1 < i < length(layers) + 1
+            f_vars[i-1] = @variable(model, [1:n])
         end
     end
-    return neurons[1] #return input variable
+    return b_vars, f_vars
 end
 
+function add_input_constraint(model::Model, input::HPolytope, neuron_vars::Vector{Variable})
+    in_A,  in_b  = tosimplehrep(input)
+    @constraint(model,  in_A * neuron_vars .<= in_b)
+    return nothing
+end
+
+function add_input_constraint(model::Model, input::Hyperrectangle, neuron_vars::Vector{Variable})
+    @constraint(model,  neuron_vars .<= high(input))
+    @constraint(model,  neuron_vars .>= low(input))
+    return nothing
+end
+
+function add_output_constraint(model::Model, output::AbstractPolytope, neuron_vars::Vector{Variable})
+    out_A, out_b = tosimplehrep(output)
+    @constraint(model, out_A * neuron_vars .<= out_b)
+    return nothing
+end
+
+function encode(model::Model, problem::Problem, relustatus::Array{Array{Int64,1},1})
+    #bs, fs = init_nnet_vars(model, problem.network)
+    
+    #TEST
+    bs = [[@variable(m, b11)],
+         [@variable(m, b21),@variable(m, b22)],
+         [@variable(m, b31),@variable(m, b32)],  
+         [@variable(m, b41)]]
+
+    fs = [[@variable(m, f21),@variable(m, f22)],
+          [@variable(m, f31),@variable(m, f32)]]
+    
+    #add_input_constraint(model, inputSet, bs[1])
+    #add_output_constraint(model, problem.output, last(bs))
+
+    for (i, layer) in enumerate(problem.network.layers)
+        #print(string("i: ",i))
+        (W, b, act) = (layer.weights, layer.bias, layer.activation)
+        #before_act = W * neurons[i] + b
+        
+        #first layer is different
+        if i == 1
+            for j in 1:length(layer.bias)
+               @constraint(model, -bs[2][j] + bs[1][1]*W[j] == -b[j]) 
+            end
+        elseif 1<i
+            for j in 1:length(layer.bias) # For evey node
+                @constraint(model, -bs[i+1][j] + dot(fs[i-1],W[j,:]) == -b[j])
+            end
+        end
+    end
+
+    # Adding linear constraints
+    
+    #first layer
+    bounds = get_bounds(problem)
+    
+    
+    #@constraint(model, bs[1] .<= bounds[1].center + bounds[1].radius)
+    #@constraint(model, bs[1] .>= bounds[1].center - bounds[1].radius)
+    
+    for i in 1:length(bs)
+        @constraint(model, bs[i] .<= bounds[i].center + bounds[i].radius)
+        @constraint(model, bs[i] .>= bounds[i].center - bounds[i].radius)
+    end
+    
+    # positivity contraint for f variables
+    for i in 1:length(fs)
+        @constraint(model, fs[i] .>= zeros(length(fs[i])))
+    end
+    # Objective: L∞ norm of the disturbance\
+    @objective(m, Max, 0)
+    return (bs, fs)
+end
+
+function check_relu_status(bs::Array{Array{JuMP.Variable,1},1}, 
+        fs::Array{Array{JuMP.Variable,1},1})
+    b_values = [getvalue(b) for b in bs[2:length(bs)-1]]
+    f_values = [getvalue(f) for f in fs]
+    return [x[1] .== x[2] for x in zip(b_values, f_values)]
+end
