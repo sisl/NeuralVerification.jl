b64ccc157e35e75d7eca12e8ea1d965f5a266bb4
tomer arnon <tomerarnon@gmail.com>
2018-11-14 13:34:10 -0800

    move dual_cost function to duality and simplify a couple of functions

--- src/optimization/utils/objectives.jl
+++ src/optimization/utils/objectives.jl
@@ -11,24 +11,12 @@ function max_disturbance(model::Model, var)
 end
 
 function min_sum_all(model::Model, var)
-    # J = 0.0
-    # for i in 1:length(var)
-    #     for j in 1:length(var[i])
-    #         J += var[i][j]
-    #     end
-    # end
     J = sum(sum.(var))
     @objective(model, Min, J)
     return J
 end
 
 function max_sum_all(model::Model, var)
-    # J = 0.0
-    # for i in 1:length(var)
-    #     for j in 1:length(var[i])
-    #         J += var[i][j]
-    #     end
-    # end
     J = sum(sum.(var))
     @objective(model, Max, J)
     return J
@@ -36,18 +24,4 @@ end
 
 function zero_objective(model::Model)
     @objective(model, Max, 0.0)
-end
-
-# NOTE: if this is only used in duality, it should probably be  defined there.
-# particularly since dual_cost for convDual is defined differently
-function dual_cost(model::Model, nnet::Network, bounds::Vector{Hyperrectangle}, λ, μ)
-    J = input_layer_cost(nnet.layers[1], μ[1], bounds[1])
-    for i in 2:length(nnet.layers)
-        J += layer_cost(nnet.layers[i], μ[i], λ[i-1], bounds[i])
-    end
-    for i in 1:length(nnet.layers)
-        J += activation_cost(nnet.layers[i], μ[i], λ[i], bounds[i])
-    end
-    @objective(model, Min, J)
-    return J
 end
