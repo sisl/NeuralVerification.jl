55b0a1bdf406d41b1472d9c3f1e67cce9c959362
changliuliu <changliu.liu@le.com>
2018-12-09 20:16:35 -0800

    clean sherlock

--- src/satisfiability/sherlock.jl
+++ src/satisfiability/sherlock.jl
@@ -17,11 +17,12 @@ function solve(solver::Sherlock, problem::Problem)
 end
 
 function output_bound(solver::Sherlock, problem::Problem, type::Symbol)
+    opt = solver.optimizer
     x = sample(problem.input)
     while true
-        (x, bound) = local_search(solver, problem, x, type)
+        (x, bound) = local_search(problem, x, opt, type)
         bound += ifelse(type == :max, solver.ϵ, -solver.ϵ)
-        (x_new, bound_new, feasibile) = global_search(problem, solver, bound, type)
+        (x_new, bound_new, feasibile) = global_search(problem, bound, opt, type)
         if feasibile
             (x, bound) = (x_new, bound_new)
         else
@@ -36,43 +37,33 @@ function sample(set::AbstractPolytope)
     return x[1]
 end
 
-function local_search(solver::Sherlock, problem::Problem, x::Vector{Float64}, type::Symbol)
+function local_search(problem::Problem, x::Vector{Float64}, optimizer::AbstractMathProgSolver, type::Symbol)
     nnet = problem.network
-
     act_pattern = get_activation(nnet, x)
     gradient = get_gradient(nnet, x)
-
-    model = Model(solver = solver.optimizer)
-
+    model = Model(solver = optimizer)
     neurons = init_neurons(model, nnet)
     add_input_constraint(model, problem.input, first(neurons))
     encode_lp(model, nnet, act_pattern, neurons)
-
     J = gradient * neurons[1]
     index = ifelse(type == :max, 1, -1)
     @objective(model, Max, index * J[1])
-
     solve(model)
-
     x_new = getvalue(neurons[1])
     bound_new = compute_output(nnet, x_new)
     return (x_new, bound_new[1])
 end
 
-global_search(problem::Problem, solver::Sherlock, bound::Float64, type::Symbol) = global_search(problem.network, problem.input, solver.optimizer, bound, type)
-
-function global_search(nnet::Network, input::AbstractPolytope, optimizer::AbstractMathProgSolver, bound::Float64, type::Symbol)
-    # Call Reverify for global search
+function global_search(problem::Problem, bound::Float64, optimizer::AbstractMathProgSolver, type::Symbol)
     index = ifelse(type == :max, 1.0, -1.0)
     h = HalfSpace([index], index * bound)
     output_set = HPolytope([h])
-
-    problem = Problem(nnet, input, output_set)
+    problem_new = Problem(problem.network, problem.input, output_set)
     solver  = NSVerify(optimizer)
-    result  = solve(solver, problem)
+    result  = solve(solver, problem_new)
     if result.status == :UNSAT
         x = result.counter_example
-        bound = compute_output(nnet, x)
+        bound = compute_output(problem.network, x)
         return (x, bound[1], true)
     else
         return ([], 0.0, false)
