e59606444ed9fcb7c23290e2f10d57ed948046a3
tomer arnon <tomerarnon@gmail.com>
2018-12-16 22:59:01 -0800

    add SparseArrays dependency (probably temporary)

--- src/NeuralVerification.jl
+++ src/NeuralVerification.jl
@@ -14,6 +14,7 @@ using Reexport
 @reexport using Polyhedra
 @reexport using CDDLib
 @reexport using LinearAlgebra
+@reexport using SparseArrays
 
 import LazySets: dim, HalfSpace # dim is necessary to avoid conflict with Polyhedra.dim, HalfSpace is not defined unless imported
 
