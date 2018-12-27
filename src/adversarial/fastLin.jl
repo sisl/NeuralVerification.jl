7caa22695ec0576c20dd25b1c9fc6bf7b9487ae0
tomer arnon <tomerarnon@gmail.com>
2018-12-27 03:20:59 -0800

    addresses #6

--- src/adversarial/fastLin.jl
+++ src/adversarial/fastLin.jl
@@ -57,109 +57,116 @@ function solve(solver::FastLin, problem::Problem)
     end
 end
 
-function forward_network(ϵ::Float64, nnet::Network, input::Hyperrectangle)
-
-    len = length(nnet.layers)
-
-    act_pattern = Vector{Float64}()
-    A = Vector{Matrix{Float64}}()
-    l = Vector{Vector{Float64}}()
-    u = Vector{Vector{Float64}}()
-
-    x0 = input.center
-    println("in FN")
-    for m in 1:len
-        println("looping $m")
-        new_l, new_u = get_next_bounds(nnet.layers, x0, ϵ, A, l, act_pattern)
-
-        update_A!(A, m, new_l, new_u)
-
-        push!(l, new_l)
-        push!(u, new_u)
-    end
-
-    return Hyperrectangle(low = last(l), high = last(u))
+function forward_network(solver::FastLin, nnet::Network, input::Hyperrectangle)
+    # use MaxSens to compute bounds:
+    L, U = get_bounds(nnet, input.center, input.radius[1])
+    output = Hyperrectangle(low = last(L), high = last(U))
+    return output
 end
 
-#=
-input: input data vector,
-p : ℓp norm,
-ϵ: maximum ℓp-norm perturbation
-l[k], u[k], k∈[m] : layer-wise bounds
-=#
-function get_next_bounds(layers, input_center, ϵ, A, l, act_pattern)
-    W0 = layers[1].weights
-    m = length(A) + 1   # A is of length 0 on the first pass, and is not necessary then.
+# function forward_network(ϵ::Float64, nnet::Network, input::Hyperrectangle)
 
-    T, H = get_TH(l, A, act_pattern)
+#     len = length(nnet.layers)
 
-    ## TODO can the order of the for loops be reversed?
-    for j in 1:n_nodes(layers[m])  # for each node `j` in the mth layer:
-        vj = W0[j, :]*input_center + layers[m].bias[j]
-        μ_upper = μ_lower = 0
-        for k in 1:m-1
-            μ_upper = μ_upper - A[k][j, :] * T[k][:, j]     # Eq 6
-            μ_lower = μ_lower - A[k][j, :] * H[k][:, j]
-            vj += A[k][j, :] * layers[m].bias               # Eq 7
-        end
+#     act_pattern = Vector{Float64}()
+#     A = Vector{Matrix{Float64}}()
+#     l = Vector{Vector{Float64}}()
+#     u = Vector{Vector{Float64}}()
 
-        γᵁ = vj + ϵ*sum(abs, W0[j, :]) + μ_upper     #||A[0][j, :]||[q] => qnorm of A
-        γᴸ = vj + ϵ*sum(abs, W0[j, :]) + μ_lower
-    end
+#     x0 = input.center
+#     println("in FN")
+#     for m in 1:len
+#         println("looping $m")
+#         new_l, new_u = get_next_bounds(nnet.layers, x0, ϵ, A, l, act_pattern)
 
-    push!(act_pattern, relaxed_ReLU.(γᴸ, γᵁ))
+#         update_A!(A, m, new_l, new_u)
 
-    return γᴸ, γᵁ
-end
+#         push!(l, new_l)
+#         push!(u, new_u)
+#     end
 
-function get_TH(l, A, act_pattern)
-    T = Vector{SparseMatrixCSC{Float64}}(undef, length(l))
-    H = Vector{SparseMatrixCSC{Float64}}(undef, length(l))
+#     return Hyperrectangle(low = last(l), high = last(u))
+# end
 
-    for k in 1:length(l)  # for all previous layers, (current one not included)
+# #=
+# input: input data vector,
+# p : ℓp norm,
+# ϵ: maximum ℓp-norm perturbation
+# l[k], u[k], k∈[m] : layer-wise bounds
+# =#
+# function get_next_bounds(layers, input_center, ϵ, A, l, act_pattern)
+#     W0 = layers[1].weights
+#     m = length(A) + 1   # A is of length 0 on the first pass, and is not necessary then.
 
-        nᵏ = length(act_pattern[k]) # number nodes in the kth layer
-        lᵏ = l[k]
+#     T, H = get_TH(l, A, act_pattern)
 
-        T[k] = spzeros(length(lᵏ), nᵏ) # reset/initialize each one
-        H[k] = spzeros(length(lᵏ), nᵏ) # reset/initialize each one
+#     ## TODO can the order of the for loops be reversed?
+#     for j in 1:n_nodes(layers[m])  # for each node `j` in the mth layer:
+#         vj = W0[j, :]*input_center + layers[m].bias[j]
+#         μ_upper = μ_lower = 0
+#         for k in 1:m-1
+#             μ_upper = μ_upper - A[k][j, :] * T[k][:, j]     # Eq 6
+#             μ_lower = μ_lower - A[k][j, :] * H[k][:, j]
+#             vj += A[k][j, :] * layers[m].bias               # Eq 7
+#         end
 
-        for r in 1:length(lᵏ) # NOTE: what does the length of the lower bound represent? The dimensionality of the input space? In the paper they use Iᵏ
-            if 0.0 < act_pattern[k][r] < 1.0
-                # A⁺ = findall(...)  ## for Julia 0.7+
-                A⁺ᵣ = find(a->a>0, A[k][:, r])            # positive indices
-                A⁻ᵣ = setdiff(eachindex(A[k][:, r]), A⁺)  # negative indices
-                T[k][r, A⁺] = lᵏ[A⁺ᵣ]
-                H[k][r, A⁻] = lᵏ[A⁻ᵣ]
-            end
-        end
-    end
-    return T, H
-end
+#         γᵁ = vj + ϵ*sum(abs, W0[j, :]) + μ_upper     #||A[0][j, :]||[q] => qnorm of A
+#         γᴸ = vj + ϵ*sum(abs, W0[j, :]) + μ_lower
+#     end
 
-# function update_A!(A, m, l, u)
+#     push!(act_pattern, relaxed_ReLU.(γᴸ, γᵁ))
 
-#     for k in reverse(1:m-1)
-#         if k == m-1
-#             D = get_D(l[m-1], u[m-1])  # diagonal matrix according to Eq. 5
-#             A[m-1] = layers[m].weights * D
-#         else
-#             A[k] = A[m-1] * A[k]
+#     return γᴸ, γᵁ
+# end
+
+# function get_TH(l, A, act_pattern)
+#     T = Vector{SparseMatrixCSC{Float64}}(undef, length(l))
+#     H = Vector{SparseMatrixCSC{Float64}}(undef, length(l))
+
+#     for k in 1:length(l)  # for all previous layers, (current one not included)
+
+#         nᵏ = length(act_pattern[k]) # number nodes in the kth layer
+#         lᵏ = l[k]
+
+#         T[k] = spzeros(length(lᵏ), nᵏ) # reset/initialize each one
+#         H[k] = spzeros(length(lᵏ), nᵏ) # reset/initialize each one
+
+#         for r in 1:length(lᵏ) # NOTE: what does the length of the lower bound represent? The dimensionality of the input space? In the paper they use Iᵏ
+#             if 0.0 < act_pattern[k][r] < 1.0
+#                 # A⁺ = findall(...)  ## for Julia 0.7+
+#                 A⁺ᵣ = find(a->a>0, A[k][:, r])            # positive indices
+#                 A⁻ᵣ = setdiff(eachindex(A[k][:, r]), A⁺)  # negative indices
+#                 T[k][r, A⁺] = lᵏ[A⁺ᵣ]
+#                 H[k][r, A⁻] = lᵏ[A⁻ᵣ]
+#             end
 #         end
 #     end
+#     return T, H
 # end
-# Identical to:
-# function update_A!(A, m, l, u)
-#     D = get_D(l, u)  # diagonal matrix according to Eq. 5
-#     A[m-1] = layers[m].weights * D
-#     for k in 1:m-2
-#         A[k] = A[m-1] * A[k]
-#     end
-# end
-function update_A!(A, l, u)
-    D = Diagonal(relaxed_ReLU.(l, u))  # diagonal matrix according to Eq. 5
-    WD = layers[m].weights * D
 
-    map!(a->WD*a, A, A)
-    push!(A, WD)  # consider pushing I and mapping WD onto it along with everything else
-end
+# # function update_A!(A, m, l, u)
+
+# #     for k in reverse(1:m-1)
+# #         if k == m-1
+# #             D = get_D(l[m-1], u[m-1])  # diagonal matrix according to Eq. 5
+# #             A[m-1] = layers[m].weights * D
+# #         else
+# #             A[k] = A[m-1] * A[k]
+# #         end
+# #     end
+# # end
+# # Identical to:
+# # function update_A!(A, m, l, u)
+# #     D = get_D(l, u)  # diagonal matrix according to Eq. 5
+# #     A[m-1] = layers[m].weights * D
+# #     for k in 1:m-2
+# #         A[k] = A[m-1] * A[k]
+# #     end
+# # end
+# function update_A!(A, l, u)
+#     D = Diagonal(relaxed_ReLU.(l, u))  # diagonal matrix according to Eq. 5
+#     WD = layers[m].weights * D
+
+#     map!(a->WD*a, A, A)
+#     push!(A, WD)  # consider pushing I and mapping WD onto it along with everything else
+# end
