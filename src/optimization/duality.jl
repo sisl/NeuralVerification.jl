7a52f3084ae7f35a3348b303b98ed53dc027dbc0
tomer arnon <tomerarnon@gmail.com>
2019-01-04 03:51:41 -0800

    add type restrictions, reorganize, split methods into multiple lines, delete old code

--- src/optimization/duality.jl
+++ src/optimization/duality.jl
@@ -32,11 +32,10 @@ end
 
 function solve(solver::Duality, problem::Problem)
     model = Model(solver = solver.optimizer)
-    bounds = get_bounds(problem)
     c, d = tosimplehrep(problem.output)
     λ = init_multipliers(model, problem.network)
     μ = init_multipliers(model, problem.network)
-    o = dual_value(solver, model, problem.network, bounds, λ, μ)
+    o = dual_value(solver, problem, model, λ, μ)
     @constraint(model, last(λ) .== -c)
     status = solve(model, suppress_warnings = true)
     return interpret_result(solver, status, o - d[1])
@@ -49,50 +48,13 @@ function interpret_result(solver::Duality, status, o)
     return BasicResult(:UNSAT)
 end
 
-# For each layer l and node k
-# max { mu[l][k] * z - lambda[l][k] * act(z) }
-function activation_value(layer, μ, λ, bound)
-    o = zero(typeof(first(μ)))
-    # (W, b, act) = (layer.weights, layer.bias, layer.activation)
-    b_hat = linear_transformation(layer, bound)
-    l_hat, u_hat = low(b_hat), high(b_hat)
-    l, u = layer.activation(l_hat), layer.activation(u_hat)
-    for k in 1:length(l)
-        o += symbolic_max(μ[k]*l_hat[k], μ[k]*u_hat[k])
-        o += symbolic_max(λ[k]*l[k], λ[k]*u[k])
-        # z = W[k, :]' * bound.center + b[k]
-        # r = sum(abs.(W[k, :]) .* bound.radius)
-        # high = μ[k] * (z + r) - λ[k] * act(z + r)
-        # low = μ[k] * (z - r) - λ[k] * act(z - r)
-        # o += symbolic_max(high, low)
-    end
-    return o
-end
-
-# For all layer l
-# max { λ[l-1]' * x[l] - μ[l]' * (W[l] * x[l] + b[l]) }
-# x[i] belongs to a Hyperrectangle
-# TODO consider bringing in μᵀ instead of μ
-function layer_value(layer, μ, λ, bound)
-    (W, b) = (layer.weights, layer.bias)
-    o = λ' * bound.center - μ' * (W * bound.center + b)
-    # instead of for-loop:
-    o += sum(symbolic_abs(λ - W' * μ) .* bound.radius) # TODO check that this is equivalent to before
-    return o
-end
-
-# Input constraint
-# max { - mu[1]' * (W[1] * input + b[1]) }
-# input belongs to a Hyperrectangle
-function input_layer_value(layer, μ, input)
-    W, b = layer.weights, layer.bias
-    o = - μ' * (W * input.center .+ b)
-    o += sum(symbolic_abs.(μ' * W) .* input.radius)   # TODO check that this is equivalent to before
-    return o
-end
-
-function dual_value(solver::Duality, model::Model, nnet::Network, bounds::Vector{Hyperrectangle}, λ, μ)
-    layers = nnet.layers
+function dual_value(solver::Duality,
+                    problem::Problem,
+                    model::Model,
+                    λ::Vector{Vector{Variable}},
+                    μ::Vector{Vector{Variable}})
+    bounds = get_bounds(problem)
+    layers = problem.network.layers
     # input layer
     o = input_layer_value(layers[1], μ[1], bounds[1])
     o += activation_value(layers[1], μ[1], λ[1], bounds[1])
@@ -102,5 +64,49 @@ function dual_value(solver::Duality, model::Model, nnet::Network, bounds::Vector
         o += activation_value(layers[i], μ[i], λ[i], bounds[i])
     end
     @objective(model, Min, o)
+    return o
+end
+
+function activation_value(layer::Layer,
+                          μᵢ::Vector{Variable},
+                          λᵢ::Vector{Variable},
+                          bound::Hyperrectangle)
+    o = zero(eltype(μᵢ))
+    b_hat = linear_transformation(layer, bound)
+    l_hat, u_hat = low(b_hat), high(b_hat)
+    l, u = layer.activation(l_hat), layer.activation(u_hat)
+
+    o += sum(symbolic_max.(μᵢ.*l_hat, μᵢ.*u_hat))
+    o += sum(symbolic_max.(λᵢ.*l, λᵢ.*u))
+
+    # also possible
+    # o += sum(@. symbolic_max(μᵢ*l_hat, μᵢ*u_hat))
+    # o += sum(@. symbolic_max(λᵢ*l, λᵢ*u))
+
+    # original
+    # for j in 1:length(l)
+    #     o += symbolic_max(μᵢ[j]*l_hat[j], μᵢ[j]*u_hat[j])
+    #     o += symbolic_max(λᵢ[j]*l[j], λᵢ[j]*u[j])
+    # end
+    return o
+end
+
+function layer_value(layer::Layer,
+                     μᵢ::Vector{Variable},
+                     λᵢ::Vector{Variable},
+                     bound::Hyperrectangle)
+    (W, b) = (layer.weights, layer.bias)
+    o = λᵢ' * bound.center - μᵢ' * (W * bound.center + b)
+    # instead of for-loop:
+    o += sum(symbolic_abs.(λᵢ .- W'*μᵢ) .* bound.radius) # TODO check that this is equivalent to before
+    return o
+end
+
+function input_layer_value(layer::Layer,
+                           μᵢ::Vector{Variable},
+                           input::Hyperrectangle)
+    W, b = layer.weights, layer.bias
+    o = -μᵢ' * (W*input.center .+ b)
+    o += sum(symbolic_abs.(μᵢ'*W) .* input.radius)   # TODO check that this is equivalent to before
     return o
 end
