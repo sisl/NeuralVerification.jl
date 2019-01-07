a6a7fa654e332ddff4e00dee4b4bff87b6880860
tomer arnon <tomerarnon@gmail.com>
2019-01-06 18:01:21 -0800

    update ignore, docs/make, travis

--- docs/make.jl
+++ docs/make.jl
@@ -4,3 +4,6 @@ makedocs(sitename = "NeuralVerification.jl",
          pages = ["index.md", "problem.md", "solvers.md", "functions.md", "existing_implementations.md"])
 
 
+deploydocs(
+    repo = "github.com/sisl/NeuralVerification.jl.git",
+)
