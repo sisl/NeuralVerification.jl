a0a9e13c5be8c7c0a9c26cd80d0a44ac7026d339
castrong <strongcreativesystems@gmail.com>
2020-06-29 17:35:07 -0700

    added test to runtests

--- test/write_nnet_test.jl
+++ test/write_nnet_test.jl
@@ -3,16 +3,16 @@
 
     # 3 --> 3 --> 2 --> 5
     l1 = NeuralVerification.Layer([3.0 2.0 1.0; 5.0 6.0 7.0; 8.0 9.0 10.0], [0.8; 1.0; 1.2], NeuralVerification.ReLU())
-    l2 = Layer([1.5 2.5 3.5; 4.5 6.5 7.5], [-1.0; -3.0], NeuralVerification.ReLU())
-    l3 = Layer([10.0 -1.0; -2.0 3.0; 4.0 5.0; 10.0 7.0; -3.5 -4.5], [0.0; -1.0; 0.0; 10.0; -10.0], NeuralVerification.ReLU())
+    l2 = NeuralVerification.Layer([1.5 2.5 3.5; 4.5 6.5 7.5], [-1.0; -3.0], NeuralVerification.ReLU())
+    l3 = NeuralVerification.Layer([10.0 -1.0; -2.0 3.0; 4.0 5.0; 10.0 7.0; -3.5 -4.5], [0.0; -1.0; 0.0; 10.0; -10.0], NeuralVerification.ReLU())
 
     # Write out the network
     network_file = string(tempname(), ".nnet")
-    nnet = Network([l1, l2, l3])
-    write_nnet(network_file, nnet)
+    nnet = NeuralVerification.Network([l1, l2, l3])
+    NeuralVerification.write_nnet(network_file, nnet)
 
     # Read back in the network
-    new_nnet = read_nnet(network_file)
+    new_nnet = NeuralVerification.read_nnet(network_file)
 
     @test new_nnet.layers[1].weights == l1.weights;
     @test new_nnet.layers[1].bias == l1.bias;
