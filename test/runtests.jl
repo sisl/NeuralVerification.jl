538c5256abcdf203cb79776c29b89d9605b3ca8d
castrong <strongcreativesystems@gmail.com>
2020-06-30 17:41:21 -0700

    first commit testing setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,8 @@ include("identity_network.jl")
 include("relu_network.jl")
 include("inactive_relus.jl")
 if Base.find_package("Flux") != nothing
-    include("flux.jl")
+    #include("flux.jl")
 end
 include("complements.jl")
-include("write_nnet_test.jl")
+#include("write_nnet_test.jl")
+include("run_correctness_tests.jl")
