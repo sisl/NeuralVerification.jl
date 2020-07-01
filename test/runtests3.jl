1a7db94820421b2e128333878801b3e43a4ad5cf
Tianhao <phi.wth@gmail.com>
2020-07-01 13:34:43 -0400

    update runtest3.jl

--- test/runtests3.jl
+++ test/runtests3.jl
@@ -12,48 +12,44 @@ macro no_error(ex)
     end
 end
 
-function test_R2_R2()
-    net_file = "$(@__DIR__)/../examples/networks/R2_R2.nnet"
-    # This is a 3 layer simple network mapping from R2 to R2.
-    
-    # y1 =  x1 + x2 - 1
-    # y2 = -x1 + x2 - 1
+net_file = "$(@__DIR__)/../examples/networks/R2_R2.nnet"
+# This is a 3 layer simple network mapping from R2 to R2.
 
-    # z1 = ReLU(y1)
-    # z2 = ReLU(y2)
+# y1 =  x1 + x2 - 1
+# y2 = -x1 + x2 - 1
 
-    # o1 = 1*z1 + 0*z2 + 0
-    # o2 = 0*z1 + 1*z2 + 0
+# z1 = ReLU(y1)
+# z2 = ReLU(y2)
 
-    net = read_nnet(net_file, last_layer_activation = Id())
-    
-    A = [
-        1. 0.;
-        0. 1.;
-        -1. 0.;
-        0. -1.;
-    ]
-    b = [
-        1.1
-        1.1
-        0.1
-        0.1
-    ]
-    X = Hyperrectangle(low=[-1,-1], high=[1,1])
-    # The true output set is a union of two segments : (0,0)->(0,1) U (0,0)->(1,0)
-    Y = HPolytope(A, b) # Output constraints. Hyperrectangle: -0.1, -0.1, 1.1, 1.1
-    problem = Problem(net, X, Y)
+# o1 = 1*z1 + 0*z2 + 0
+# o2 = 0*z1 + 1*z2 + 0
 
-    solver = Neurify()
-    result = solve(solver, problem)
-    @test @no_error result = solve(solver, problem)
-    result = solve(solver, problem)
-    @test result.status == :holds
+net = read_nnet(net_file, last_layer_activation = Id())
 
-    solver = ReluVal()
-    @test @no_error result = solve(solver, problem)
-    result = solve(solver, problem)
-    @test result.status == :holds
-end
+A = [
+    1. 0.;
+    0. 1.;
+    -1. 0.;
+    0. -1.;
+]
+b = [
+    1.1
+    1.1
+    0.1
+    0.1
+]
+X = Hyperrectangle(low=[-1,-1], high=[1,1])
+# The true output set is a union of two segments : (0,0)->(0,1) U (0,0)->(1,0)
+Y = HPolytope(A, b) # Output constraints. Hyperrectangle: -0.1, -0.1, 1.1, 1.1
+problem = Problem(net, X, Y)
 
-test_R2_R2()
+solver = Neurify()
+result = solve(solver, problem)
+@test @no_error result = solve(solver, problem)
+result = solve(solver, problem)
+@test result.status == :holds
+
+solver = ReluVal()
+@test @no_error result = solve(solver, problem)
+result = solve(solver, problem)
+@test result.status == :holds
