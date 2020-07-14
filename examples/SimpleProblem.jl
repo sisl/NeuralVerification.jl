1c2f56fbf6ba118e856fd46a89cd15da8d1497f9
castrong <strongcreativesystems@gmail.com>
2020-07-14 15:21:26 -0700

    example of how to set up and write a problem to a query file

--- examples/SimpleProblem.jl
+++ examples/SimpleProblem.jl
@@ -1,12 +1,8 @@
 # Setup problem
-network = NeuralVerification.read_nnet("./test/test_sets/random/medium/networks/rand_[4,8,4]_1.nnet")
-input_set = NeuralVerification.HPolytope{Float64}([NeuralVerification.HalfSpace{Float64}([1.0, 0.0, 0.0, 0.0], 0.3845579517032085), NeuralVerification.HalfSpace{Float64}([0.0, 1.0, 0.0, 0.0], 0.8331193586918577), NeuralVerification.HalfSpace{Float64}([0.0, 0.0, 1.0, 0.0], 0.6607352829885846), NeuralVerification.HalfSpace{Float64}([0.0, 0.0, 0.0, 1.0], 0.270232549769154), NeuralVerification.HalfSpace{Float64}([-1.0, 0.0, 0.0, 0.0], 0.022984248429274823), NeuralVerification.HalfSpace{Float64}([0.0, -1.0, 0.0, 0.0], 1.061033037362016), NeuralVerification.HalfSpace{Float64}([0.0, 0.0, -1.0, 0.0], 0.734914583454296), NeuralVerification.HalfSpace{Float64}([0.0, 0.0, 0.0, -1.0], 0.7494557834041424)])
-output_set = NeuralVerification.HPolytope{Float64}(NeuralVerification.HalfSpace{Float64}[NeuralVerification.HalfSpace{Float64}([1.0, 0.0, 0.0, 0.0], 0.47919249361761995), HalfSpace{Float64}([0.0, 1.0, 0.0, 0.0], 0.21600299802246092), HalfSpace{Float64}([0.0, 0.0, 1.0, 0.0], 0.13753126667274596), HalfSpace{Float64}([0.0, 0.0, 0.0, 1.0], 0.536256411404834), HalfSpace{Float64}([-1.0, 0.0, 0.0, 0.0], 0.05623084997936845), HalfSpace{Float64}([0.0, -1.0, 0.0, 0.0], -0.02937453504189702), HalfSpace{Float64}([0.0, 0.0, -1.0, 0.0], 0.7988290732045045), HalfSpace{Float64}([0.0, 0.0, 0.0, -1.0], 1.1397183250614469)])
+network = NeuralVerification.read_nnet("./examples/networks/issue_net.nnet")
+input_set = NeuralVerification.Hyperrectangle([-0.42076139039401705], [0.42346836042582625])
+output_set = NeuralVerification.Hyperrectangle([-0.13583423942984774], [0.4613520242736866])
 problem = NeuralVerification.Problem(network, input_set, output_set)
 
-# problem = NeuralVerification.query_line_to_problem("test/test_sets/random/medium/networks/rand_[4,8,4]_1.nnet test/test_sets/random/medium/input_sets/hp_hp_one_[4,8,4]_1_inputset.json test/test_sets/random/medium/output_sets/hp_hp_one_[4,8,4]_1_outputset.json")
-# println(problem.input)
-# println(problem.output)
-
-# Exact Reach issue
-println(NeuralVerification.solve(NeuralVerification.ExactReach(), problem))
+NeuralVerification.write_problem("test/test_sets/previous_issues/networks/issue_111_network2.nnet", "test/test_sets/previous_issues/input_sets/issue_111_input_set2.json", "test/test_sets/previous_issues/output_sets/issue_111_output_set2.json", problem; query_file="test/test_sets/previous_issues/query_file_previous_issues.txt")
+println(NeuralVerification.solve(NeuralVerification.Sherlock(ϵ = 0.5), problem))
