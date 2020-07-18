63d88bed0bcde16d33fbc820b1cbff049df904bc
tomer arnon <tomerarnon@gmail.com>
2020-07-18 21:10:10 +0300

    in progress commit

--- src/optimization/nsVerify.jl
+++ src/optimization/nsVerify.jl
@@ -25,23 +25,64 @@ Sound and complete.
 """
 @with_kw struct NSVerify <: Solver
     optimizer = GLPK.Optimizer
-    m::Float64 = 1000.0  # The big M in the linearization
+    m = nothing
 end
 
 function solve(solver::NSVerify, problem::Problem)
+    # Set M automatically if not already set.
+    M = solver.m == nothing ? set_automatic_M(problem) : solver.m
+
+    @show M
+
+    # Set up and solve the problem
     model = Model(solver)
     neurons = init_neurons(model, problem.network)
     deltas = init_deltas(model, problem.network)
     add_set_constraint!(model, problem.input, first(neurons))
     add_complementary_set_constraint!(model, problem.output, last(neurons))
-    encode_network!(model, problem.network, neurons, deltas, MixedIntegerLP(solver.m))
+    encode_network!(model, problem.network, neurons, deltas, MixedIntegerLP(M))
+    # encode_network!(model, problem.network, neurons, deltas, get_bounds(problem), BoundedMixedIntegerLP())
     feasibility_problem!(model)
     optimize!(model)
+
+
+   #  @show model
+
+   #  for T in list_of_constraint_types(model)
+   #     cs = all_constraints(model, T...)
+   #     println(T)
+   #     println.("\t", cs)
+   # end
+
+
     if termination_status(model) == OPTIMAL
+        # Issue a warning if M is not sufficiently large.
+        # M must be larger than any other variable in the problem.
+        # @show value.(neurons)
+        @show max_z = mapreduce(x->norm(x, Inf), max, value.(neurons))
+        @show M
+        if max_z >= M
+            @warn "M not sufficiently large. Problem may have been solved incorrectly.
+            The minimum viable value of M was found to be $max_z"
+        end
+
         return CounterExampleResult(:violated, value.(first(neurons)))
-    end
-    if termination_status(model) == INFEASIBLE
+
+    elseif termination_status(model) == INFEASIBLE
         return CounterExampleResult(:holds)
+    else
+        return CounterExampleResult(:unknown)
     end
-    return CounterExampleResult(:unknown)
+end
+
+function set_automatic_M(problem)
+    # new_nnet = Network([Layer(L.weights, L.bias, Id()) for L in problem.network.layers])
+    # new_problem = Problem(new_nnet, problem.input, problem.output)
+
+    flat = Iterators.flatten
+    # bounds = get_bounds(new_problem)
+    bounds = get_bounds(problem)
+    M = maximum(abs, flat(hr.center + hr.radius for hr in bounds))
+    # add 1% margin?
+    # M *= 1.1
 end
