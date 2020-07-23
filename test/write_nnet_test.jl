43a0179aa123df8705aaa4ab92b70ea914fcc932
castrong <strongcreativesystems@gmail.com>
2020-07-23 16:12:31 -0700

    disambiguate objects in write_nnet_test.jl by adding NeuralVerification in front

--- test/write_nnet_test.jl
+++ test/write_nnet_test.jl
@@ -2,16 +2,16 @@
 @testset "Read nnet test" begin
 
     # 3 --> 3 --> 2 --> 5
-    l1 = Layer([3.0 2.0 1.0; 5.0 6.0 7.0; 8.0 9.0 10.0], [0.8; 1.0; 1.2], ReLU())
-    l2 = Layer([1.5 2.5 3.5; 4.5 6.5 7.5], [-1.0; -3.0], ReLU())
+    l1 = NeuralVerification.Layer([3.0 2.0 1.0; 5.0 6.0 7.0; 8.0 9.0 10.0], [0.8; 1.0; 1.2], NeuralVerification.ReLU())
+    l2 = NeuralVerification.Layer([1.5 2.5 3.5; 4.5 6.5 7.5], [-1.0; -3.0], NeuralVerification.ReLU())
     # The .nnet file doesn't store an activation at each layer,
-    l3 = Layer([10.0 -1.0; -2.0 3.0; 4.0 5.0; 10.0 7.0; -3.5 -4.5], [0.0; -1.0; 0.0; 10.0; -10.0], Id())
+    l3 = NeuralVerification.Layer([10.0 -1.0; -2.0 3.0; 4.0 5.0; 10.0 7.0; -3.5 -4.5], [0.0; -1.0; 0.0; 10.0; -10.0], NeuralVerification.Id())
 
     # Write out the network
     network_file = string(tempname(), ".nnet")
     println("File: ", network_file)
-    nnet = Network([l1, l2, l3])
-    write_nnet(network_file, nnet; header_text="test_header")
+    nnet = NeuralVerification.Network([l1, l2, l3])
+    NeuralVerification.write_nnet(network_file, nnet; header_text="test_header")
 
     # Read back in the network
     new_nnet = NeuralVerification.read_nnet(network_file)
