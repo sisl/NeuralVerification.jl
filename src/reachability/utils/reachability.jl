bd2b47fc98b08db1e82df8f7a692e19b242cfe51
tomer arnon <tomerarnon@gmail.com>
2020-08-19 23:40:49 +0300

    move SymbolicInterval related stuff to reachability.jl

--- src/reachability/utils/reachability.jl
+++ src/reachability/utils/reachability.jl
@@ -44,3 +44,73 @@ function forward_partition(act::ReLU, input)
     end
     return output
 end
+
+
+
+############# For ReluVal and Neurify
+
+struct SymbolicInterval{F<:AbstractPolytope}
+    Low::Matrix{Float64}
+    Up::Matrix{Float64}
+    domain::F
+end
+
+# Data to be passed during forward_layer
+struct SymbolicIntervalGradient{F<:AbstractPolytope, N<:Real}
+    sym::SymbolicInterval{F}
+    LΛ::Vector{Vector{N}}
+    UΛ::Vector{Vector{N}}
+end
+# Data to be passed during forward_layer
+const SymbolicIntervalMask = SymbolicIntervalGradient{<:Hyperrectangle, Bool}
+
+function _init_symbolic_grad_general(domain, N)
+    n = dim(domain)
+    I = Matrix{Float64}(LinearAlgebra.I(n))
+    Z = zeros(n)
+    symbolic_input = SymbolicInterval([I Z], [I Z], domain)
+    symbolic_mask = SymbolicIntervalGradient(symbolic_input,
+                                             Vector{Vector{N}}(),
+                                             Vector{Vector{N}}())
+end
+function init_symbolic_grad(domain)
+    VF = Vector{HalfSpace{Float64, Vector{Float64}}}
+    domain = HPolytope(VF(constraints_list(domain)))
+    _init_symbolic_grad_general(domain, Float64)
+end
+function init_symbolic_mask(interval)
+    _init_symbolic_grad_general(interval, Bool)
+end
+
+
+
+domain(sym::SymbolicInterval) = sym.domain
+domain(grad::SymbolicIntervalGradient) = domain(grad.sym)
+_sym(sym::SymbolicInterval) = sym
+_sym(grad::SymbolicIntervalGradient) = grad.sym
+
+upper(sym::SymbolicInterval) = AffineMap(@view(sym.Up[:, 1:end-1]), sym.domain, @view(sym.Up[:, end]))
+lower(sym::SymbolicInterval) = AffineMap(@view(sym.Low[:, 1:end-1]), sym.domain, @view(sym.Low[:, end]))
+upper(grad::SymbolicIntervalGradient) = upper(grad.sym)
+lower(grad::SymbolicIntervalGradient) = lower(grad.sym)
+
+upper_bound(a::AbstractVector, set::LazySet) = a'σ(a, set)
+lower_bound(a::AbstractVector, set::LazySet) = a'σ(-a, set) # ≡ -ρ(-a, set)
+bounds(a::AbstractVector, set::LazySet) = (a'σ(-a, set), a'σ(a, set))  # (lower, upper)
+
+upper_bound(S::LazySet, j::Integer) = upper_bound(Arrays.SingleEntryVector(j, dim(S), 1.0), S)
+lower_bound(S::LazySet, j::Integer) = lower_bound(Arrays.SingleEntryVector(j, dim(S), 1.0), S)
+bounds(S::LazySet, j::Integer) = (lower_bound(S, j), upper_bound(S, j))
+
+const _SymIntOrGrad = Union{SymbolicInterval, SymbolicIntervalGradient}
+dim(sym::_SymIntOrGrad) = size(_sym(sym).Up, 1)
+high(sym::_SymIntOrGrad) = σ(ones(dim(sym)), upper(sym))
+low(sym::_SymIntOrGrad) = σ(-ones(dim(sym)), lower(sym))
+# radius of the symbolic interval in the direction of the
+# jth generating vector. This is not the axis aligned radius,
+# or the bounding radius, but rather a radius with respect to
+# a node in the network. Equivalent to the upper-upper
+# bound minus the lower-lower bound
+function radius(sym::_SymIntOrGrad, j::Integer)
+    upper_bound(upper(sym), j) - lower_bound(lower(sym), j)
+end
