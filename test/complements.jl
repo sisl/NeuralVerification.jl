63b65418bd2e92a5f9d68c0a656fa50b9c4d8d56
castrong <strongcreativesystems@gmail.com>
2021-04-29 12:39:27 -0700

    complement test update to qualify complement function with NeuralVerification.

--- test/complements.jl
+++ test/complements.jl
@@ -9,8 +9,8 @@
         @test [5.0, 5.0]   ∈ HS && [5.0, 5.0]   ∉ PC
         @test [-1.0, -1.0] ∉ HS && [-1.0, -1.0] ∈ PC
 
-        @test complement(PC) == HS
-        @test complement(HS) == PC
+        @test NeuralVerification.complement(PC) == HS
+        @test NeuralVerification.complement(HS) == PC
 
         # Hyperrectangle contained in HS
         hr = Hyperrectangle(low = [1.0, 1.0], high = [2.0, 2.0])
@@ -28,11 +28,11 @@
         @test !(hr ⊆ HS)
 
         # Test some other sets
-        @test @no_error complement(Hyperrectangle(ones(2), ones(2)))
-        @test @no_error complement(Ball2(ones(2), 1.0))
-        @test @no_error complement(Ball1(ones(3), 1.0))
-        @test @no_error complement(Zonotope(ones(4), ones(4, 2)))
-        @test @no_error complement(convert(HPolytope, hr))
+        @test @no_error NeuralVerification.complement(Hyperrectangle(ones(2), ones(2)))
+        @test @no_error NeuralVerification.complement(Ball2(ones(2), 1.0))
+        @test @no_error NeuralVerification.complement(Ball1(ones(3), 1.0))
+        @test @no_error NeuralVerification.complement(Zonotope(ones(4), ones(4, 2)))
+        @test @no_error NeuralVerification.complement(convert(HPolytope, hr))
     end
 
     @testset "Solvers with PCs" begin
