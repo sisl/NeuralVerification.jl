2d13e46f59d962dfca16da9e15c684e5311bd563
castrong <strongcreativesystems@gmail.com>
2021-05-10 11:30:05 -0700

    switched to LazySets complement instead of our own PolytopeComplement

--- src/NeuralVerification.jl
+++ src/NeuralVerification.jl
@@ -40,8 +40,6 @@ export
     Solver,
     Network,
     AbstractActivation,
-    PolytopeComplement,
-    complement,
     # NOTE: not sure if exporting these is a good idea as far as namespace conflicts go:
     # ReLU,
     # Max,
